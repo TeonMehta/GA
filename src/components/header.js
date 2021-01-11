ab9d9df257e66939d6e4717a5290f0a56f6a7287
TeonMehta <teonmehta@gmail.com>
2021-01-10 21:50:28 -0500

    header 0.5

--- src/components/header.js
+++ src/components/header.js
@@ -1,38 +1,57 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+// import logo from "../images/logo.png"
+import './header.css'
+import Image from "./image"
+
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <header>
+    <div className="nav">
+      <div className="brand"><Link to='/'><Image /> </Link></div>
+      <div className="menu-wrapper">
+        <ul className="menu-list">
+          <li className="menu-item"><Link to='/about'>About Us</Link></li>
+          <li className="menu-item">
+            <Link to='/' className='dropdown-link'>Business</Link>
+            <div className="dropdown-wrapper">
+              <h3 className="dropdown-heading">Business</h3>
+              <ul className="dropdown">
+                <li className="sub-menu-item"><Link to='/corporate-tax'>Corporate Tax</Link></li>
+                <li className="sub-menu-item"><Link to='/'>Financial Statements</Link></li>
+                <li className="sub-menu-item"><Link to='/'>Bookkeeping</Link></li>
+                <li className="sub-menu-item"><Link to='/'>Payroll</Link></li>
+                <li className="sub-menu-item"><Link to='/'>New Business Formation</Link></li>
+                <li className="sub-menu-item"><Link to='/'>CRA Audits</Link></li>
+              </ul>
+            </div>
+
+          </li>
+          <li className="menu-item">
+            <Link to='/' className='dropdown-link'>Personal</Link>
+            <div className="dropdown-wrapper">
+              <h3 className="dropdown-heading">Personal</h3>
+              <ul className="dropdown">
+                <li className="sub-menu-item"><Link to='/'>Personal Tax</Link></li>
+                <li className="sub-menu-item"><Link to='/'>Personal Financial Planning</Link></li>
+              </ul>
+            </div>
+
+          </li>
+          <li className="menu-item"><Link to='/faq'>FAQ</Link></li>
+        </ul>
+      </div>
+      <div className="contact-btn-wrapper">
+        <Link to='/contact' className='contact-btn'>Contact Us <span>&#8594;</span></Link>
+      </div>
     </div>
   </header>
 )
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
+
 }
 
 Header.defaultProps = {
---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+import {Link} from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const About = () => {
+  return (
+    <div>
+      About
+    </div>
+  )
+}
+
+export default About
