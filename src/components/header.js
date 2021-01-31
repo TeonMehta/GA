9dd67aee19b4fdd6c0d954342d7b8915715e17f8
TeonMehta <teonmehta@gmail.com>
2021-01-31 10:19:33 -0500

    retackle project

--- src/components/header.js
+++ src/components/header.js
@@ -8,42 +8,44 @@ import Image from "./image"
 
 const Header = ({ siteTitle }) => (
   <nav>
-    <div className="nav">
-      <div className="brand"><Link to='/'><Image /> </Link></div>
-      <div className="menu-wrapper">
-        <ul className="menu-list">
-          <li className="menu-item"><Link to='/about'>About Us</Link></li>
-          <li className="menu-item">
-            <Link to='/' className='dropdown-link'>Business</Link>
-            <div className="dropdown-wrapper">
-              <h3 className="dropdown-heading">Business</h3>
-              <ul className="dropdown">
-                <li className="sub-menu-item"><Link to='/corporate-tax'>Corporate Tax</Link></li>
-                <li className="sub-menu-item"><Link to='/'>Financial Statements</Link></li>
-                <li className="sub-menu-item"><Link to='/'>Bookkeeping</Link></li>
-                <li className="sub-menu-item"><Link to='/'>Payroll</Link></li>
-                <li className="sub-menu-item"><Link to='/'>New Business Formation</Link></li>
-                <li className="sub-menu-item"><Link to='/'>CRA Audits</Link></li>
-              </ul>
-            </div>
+    <div className="nav-wrapper">
+      <div className="nav">
+        <div className="brand"><Link to='/'><Image /> </Link></div>
+        <div className="menu-wrapper">
+          <ul className="menu-list">
+            <li className="menu-item"><Link to='/about'>About Us</Link></li>
+            <li className="menu-item">
+              <Link to='/' className='dropdown-link'>Business</Link>
+              <div className="dropdown-wrapper">
+                <h3 className="dropdown-heading">Business</h3>
+                <ul className="dropdown">
+                  <li className="sub-menu-item"><Link to='/corporate-tax'>Corporate Tax</Link></li>
+                  <li className="sub-menu-item"><Link to='/'>Financial Statements</Link></li>
+                  <li className="sub-menu-item"><Link to='/'>Bookkeeping</Link></li>
+                  <li className="sub-menu-item"><Link to='/'>Payroll</Link></li>
+                  <li className="sub-menu-item"><Link to='/'>New Business Formation</Link></li>
+                  <li className="sub-menu-item"><Link to='/'>CRA Audits</Link></li>
+                </ul>
+              </div>
 
-          </li>
-          <li className="menu-item">
-            <Link to='/' className='dropdown-link'>Personal</Link>
-            <div className="dropdown-wrapper">
-              <h3 className="dropdown-heading">Personal</h3>
-              <ul className="dropdown">
-                <li className="sub-menu-item"><Link to='/'>Personal Tax</Link></li>
-                <li className="sub-menu-item"><Link to='/'>Personal Financial Planning</Link></li>
-              </ul>
-            </div>
+            </li>
+            <li className="menu-item">
+              <Link to='/' className='dropdown-link'>Personal</Link>
+              <div className="dropdown-wrapper">
+                <h3 className="dropdown-heading">Personal</h3>
+                <ul className="dropdown">
+                  <li className="sub-menu-item"><Link to='/'>Personal Tax</Link></li>
+                  <li className="sub-menu-item"><Link to='/'>Personal Financial Planning</Link></li>
+                </ul>
+              </div>
 
-          </li>
-          <li className="menu-item"><Link to='/faq'>FAQ</Link></li>
-        </ul>
-      </div>
-      <div className="contact-btn-wrapper">
-        <Link to='/contact' className='contact-btn'>Contact Us <span>&#8594;</span></Link>
+            </li>
+            <li className="menu-item"><Link to='/faq'>FAQ</Link></li>
+          </ul>
+        </div>
+        <div className="contact-btn-wrapper">
+          <Link to='/contact' className='contact-btn'>Contact Us <span>&#8594;</span></Link>
+        </div>
       </div>
     </div>
   </nav>
--- src/pages/about.js
+++ src/pages/about.js
@@ -2,11 +2,54 @@ import React from "react"
 import {Link} from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Header from "../components/header"
+import Footer from "../components/Footer"
 
+const qualities = [
+  {
+    label: 'Accuracy',
+    description:'In the world of business, there is no room for error. We know that that the profitability of your business depends greatly on our ability to provide you with accurate bookkeeping and write-ups. Our accountants will help you run your business efficiently by providing you with well-organized financial records.'
+  },
+  {
+    label: 'Efficiency',
+    description:'With tax and insurance laws constantly changing, you need a firm that can successfully guide you to informed decisions regarding the current economic state. Global Accounting is committed to keeping up with all the latest changes to ensure you both comply and prosper under the law.'
+  },
+  {
+    label: 'Professionalism',
+    description:'No matter how complex your financial situation, Global Accounting will be there when you need us to provide you with professional and objective financial advice. At Global Accounting Services Inc., we employ all of our expertise to ensure the growth and longevity of your wealth throughout the entire process.'
+  }
+
+];
 const About = () => {
   return (
     <div>
-      About
+      <Header/>
+      <div className="about-us-wrapper">
+        <div className="about-us container">
+          <h1>We Are Global Accounting Brampton</h1>
+          <div className="about image">img</div>
+          <div className="about info">
+            <div className="column info-1">Throughout the years, computerized accounting software may have changed the way we conduct business, but there are a few things that have always remained the same. We strive to guide our clients through complex financial entanglements in a straightforward and non-intimidating manner.</div>
+            <div className="column info-2">Whether you need assistance with retirement planning or estate planning, or have recently started a business and are in need of our bookkeeping or financial forecasting services, we deliver solutions based on your specific situation. Not only does our comprehensive approach allow us to provide our clients with constructive financial solutions, it also gives us the opportunity to create life-long relationships.</div>
+          </div>
+          <div className="about stats"></div>
+          <div className="about partners"></div>
+          <div className="about reviews"></div>
+          <div className="about qualities">
+            {qualities.map((quality) =>
+              <div className='wrapper'>
+                <div className="label">
+                  {quality.label}
+                </div>
+                .
+
+              </div>
+            )}
+          </div>
+          <div className="about team"></div>
+        </div>
+      </div>
+      <Footer/>
     </div>
   )
 }
